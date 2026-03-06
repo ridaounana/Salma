a98f5fed1e975332c0a53518124588cd8e25a878
ovh <ounana.rida@gmail.Com>
2026-03-06 22:06:32 +0100

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3709839303" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3488724261" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
